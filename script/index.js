4d3038793992f9afd4581e9763adbe03f6ed77ab
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-02-23 22:31:54 +0800

    вторая итерация

--- script/index.js
+++ script/index.js
@@ -1,27 +1,30 @@
 let profileButton = document.querySelector(".profile__edit-button");
 let popup = document.querySelector(".popup");
 let popupClose = document.querySelector(".popup__close");
-let formElement = document.querySelector(".popup__container")
+let formElement = document.querySelector(".popup__form");
 let nameInput = formElement.querySelector(".popup__input_type_name");
 let jobInput = formElement.querySelector(".popup__input_type_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__profession");
+let buttonSave = document.querySelector(".popup__button");
 
-profileButton.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
+profileButton.addEventListener("click", function () {
+  popup.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
 
-popupClose.addEventListener('click', function () {
-  popup.classList.remove('popup_opened');
+popupClose.addEventListener("click", function () {
+  popup.classList.remove("popup_opened");
 });
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value; 
-    popup.classList.remove('popup_opened');
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove("popup_opened");
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+buttonSave.addEventListener("click", handleFormSubmit);
+
+formElement.addEventListener("submit", handleFormSubmit);
