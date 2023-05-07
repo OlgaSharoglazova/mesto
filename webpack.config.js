ffec8fe357b458c5da7feda31c0da83919996e69
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-05-07 16:11:14 +0800

    верстка

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,24 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
-      {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
-      },
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'images/[name].[hash][ext]',
+      }
+    },
+    {
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'fonts/[name].[hash][ext]',
+      }
+    }, 
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
