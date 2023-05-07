4fe458da3bdd52e58f9cebd31fe602de4ad7537a
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-05-07 16:40:41 +0800

    пр9 верстка корзины

---
+++ src/components/PopupWithConfirm.js
@@ -0,0 +1,12 @@
+import { Popup } from "./Popup.js";
+
+export class PopupWithConfirm extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+  }
+}  
