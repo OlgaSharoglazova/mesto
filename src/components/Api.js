06db67695ba37d6006b825748db01014fa028bf0
Sharoglazova Olga <scharoglazovao@yandex.ru>
2023-05-12 22:27:58 +0800

    отображение лайков

--- src/components/Api.js
+++ src/components/Api.js
@@ -48,7 +48,6 @@ class Api {
     return fetch(`${this._baseUrl}/cards/${id}`, {
       method: 'DELETE',
       headers: this._headers,
-    
     })
     .then(res => res.ok ? res.json() : Promise.reject(res.status))
     .catch(console.log)
